14bd44afab8081fb5263c9cd9e9bc64ea892e11e
nivmizrahi1 <nivmizrahi2000@gmail.com>
2023-07-03 20:21:35 +0300

    3

--- summer.js
+++ summer.js
@@ -1,4 +1,15 @@
-function x () {
+function checkEnglish(txt){
+    for ( let i = 0; i <= txt.length;i++)
+   { if (!(txt.charAt(i) >= "a" && txt.charAt(i)>= "z" ))
+    return false;
+}
+return true;
+}
+
+
+function clickBtn ()
+// אימות איימל ע"פ נקודה ושטרודל
+{
 const email= document.getElementById ("email").value;
 if(email.indexOf("@" ) == -1 ){
     alert ("הכנס אימייל תקין ");}
@@ -7,6 +18,10 @@ if(email.indexOf("@" ) == -1 ){
         alert ("הכנס אימייל תקין ");
 
 }}}
+if (checkEnglish (email) == false){
+alert ("אנגלית בלבד");
+return false;
+}
 
 
-document.getElementById("btn").addEventListener("click",x);
+document.getElementById("btn").addEventListener("click",clickBtn);
