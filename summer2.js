228b4c3a72ec5586822a06a49ea6c2ac2446b932
nivmizrahi1 <nivmizrahi2000@gmail.com>
2023-07-15 17:11:12 +0300

    24

--- summer2.js
+++ summer2.js
@@ -1,1 +1,12 @@
-const users = JSON.parse(localStorage.getItem('data')) || []; //// כל הנתונים ששמרנו חוזרים למשתמש , נקבל את זה כאובייקט
+const users = JSON.parse(localStorage.getItem('data')) || []; //// קישור בין הלוקאל לדף החדש 
+const regiseredUsernames= users.map (u=>u.username);
+const username= document.getElementById ('username').ariaValueMax;
+if (regiseredUsernames.indexOf(username)>-1) {
+    alert ('please choose another username'); 
+}
+var user={
+    username:'dfsfd' , 
+    password: 'asdasd'
+}
+users.push(user);
+localStorage.setItem ('users', json.stringify(users));
