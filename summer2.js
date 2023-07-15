82cdaffc92620764c0210f4fb7fac5f9157bdab7
nivmizrahi1 <nivmizrahi2000@gmail.com>
2023-07-15 18:07:15 +0300

    12

--- summer2.js
+++ summer2.js
@@ -1,4 +1,17 @@
 const users = JSON.parse(localStorage.getItem('data')) || []; //// קישור בין הלוקאל לדף החדש 
+const username= document.getElementById ('username').value;
+const password= document.getElementById('password').value;
+if (indexof (username)!=indexof (password)) {
+    alert ("there is a problem");
+}
+if (index == -1 || password != users[index].password || chance < 2) {
+    alert("Wrong username or password!") };
+
+
+
+
+
+/*
 const regiseredUsernames= users.map (u=>u.username);
 const username= document.getElementById ('username').ariaValueMax;
 if (regiseredUsernames.indexOf(username)>-1) {
